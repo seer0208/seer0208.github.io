8e3466785cffaa06975e43db39a67375a4478a67
seer0208 <seerray0208@gmail.com>
2026-03-19 19:24:43 +0800

    init astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://seer0208.github.io',
+  base: '/my-astro-site',
+});
